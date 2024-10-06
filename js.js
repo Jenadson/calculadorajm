7e2fe54e2f11fcf353d3a7cfef785cd3439dffdc
Jenadson Basilio <48029019+Jenadson@users.noreply.github.com>
2024-10-06 14:18:10 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,6 +3,9 @@ let box =  document.getElementById("mybox");
 let line = document.getElementById("myline");
 let time = document.getElementById("time");
 
+let boxJit =  document.getElementById("myboxjit");
+let lineJit = document.getElementById("mylinejit");
+let timeJit = document.getElementById("timejit");
 
 
 function calculate(){
@@ -33,3 +36,30 @@ function calculate(){
 
 
 
+function jit(){
+    
+
+    let boxlinetimejit = ((boxJit.value*0.34)+(lineJit.value*0.67))/timeJit.value;
+
+    boxlinejit =parseInt(boxlinetimejit)
+    document.getElementById("result").innerText =boxlinejit;
+
+    if (boxlinejit < 60 ) {
+        document.getElementById("scale").innerText ="Nota 5 (0% - 60%)"
+    }
+    else if (boxlinejit < 89){
+        document.getElementById("scale").innerText ="Nota 4 (61% - 89%)"
+    }
+    else if (boxlinejit < 105) {
+        document.getElementById("scale").innerText ="Nota 3 (90% - 105%)"
+    }
+    else if (boxlinejit < 120) {
+        document.getElementById("scale").innerText ="Nota 2 (106% - 120%)"
+    }
+    else {
+        document.getElementById("scale").innerText ="Nota 1 (121% - 140%)"
+    }
+
+    
+    
+};
